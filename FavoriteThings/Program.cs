5fd8683e2110d0e3608165083521330e5f2f0f8e
EvanG <evangdesigns@gmail.com>
2020-03-14 12:59:11 -0500

    Completed Motorcycles

--- FavoriteThings/Program.cs
+++ FavoriteThings/Program.cs
@@ -7,6 +7,12 @@ namespace FavoriteThings
         static void Main(string[] args)
         {
             Console.WriteLine("Cue Sound of Music");
+
+            var moto1 = new Motorcycle("Bobber", BikeType.cruiser);
+            var moto2 = new Motorcycle("Billy", BikeType.euro);
+
+            moto1.Accelerate(110);
+            moto2.Accelerate(250);
         }
     }
 }
